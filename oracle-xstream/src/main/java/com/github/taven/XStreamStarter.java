b5bebfc44ed8bf09de4ec821a9edcf7cd186a97d
TavenYin <yintianwen7@qq.com>
2021-03-29 23:30:13 +0800

    fix jar version: instantclient_12_1

--- oracle-xstream/src/main/java/com/github/taven/XStreamStarter.java
+++ oracle-xstream/src/main/java/com/github/taven/XStreamStarter.java
@@ -44,7 +44,7 @@ public class XStreamStarter {
                 config.getProperty(OracleConfig.jdbcPassword));
         OracleXStreamCDC xStreamCDC = new OracleXStreamCDC();
         xStreamCDC.start((OracleConnection) ociConnection,
-                config.getProperty(OracleConfig.outboundServer), snapshotResult.getScn());
+                config.getProperty(OracleConfig.outboundServer), 2253043);
     }
 
 }
